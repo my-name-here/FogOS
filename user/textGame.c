067ebe7f1a6dc9e196315680cd0c4ac920ac5b85
CB4747 <123422381+CB4747@users.noreply.github.com>
2024-09-20 18:26:37 -0700

    move %m to after m defined
    Finally reached new stuff

--- user/textGame.c
+++ user/textGame.c
@@ -5,11 +5,12 @@
 #include "user/user.h"
 
 int LCG(int prev){// function that generates a psuedorandom number, using the previous value in the sequence, which starts at a seed
-    prev = prev % m;
+
     //https://www.ams.org/journals/mcom/1999-68-225/S0025-5718-99-00996-5/S0025-5718-99-00996-5.pdf has table of values
     long a = 530877178;
     long c = 0;//c = 0, since it is what the chart has
     long m = 536870909;// 2^29 -3
+    prev = prev % m;
     return (a*prev+c)%m;
 }
 int isValidBet(char *bet){
