4ac636427332cd195264012e55f1805d2f6e2a06
CB4747 <123422381+CB4747@users.noreply.github.com>
2024-09-20 17:41:06 -0700

    add function to use for randomness

--- user/textGame.c
+++ user/textGame.c
@@ -4,6 +4,9 @@
 #include "kernel/fcntl.h"
 #include "user/user.h"
 
+int LCG(){// function that generates a psuedorandom number, using the previous value in the sequence, which starts at a seed
+  return 0;
+}
 int main() {
 
   return 0;
