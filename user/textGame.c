49d70a6f7ca81189461ca15eeaddf7da892b40e8
CB4747 <123422381+CB4747@users.noreply.github.com>
2024-09-20 18:05:18 -0700

    game logic

--- user/textGame.c
+++ user/textGame.c
@@ -46,6 +46,15 @@ int main() {// a reimplementation of a simple casino game I wrote in python a wh
         }
         randVal = LCG(randVal);// get a new random val with LCG
         roll = (randVal % 97) + 1; // get a roll from 1-98
+        if (roll == 1){// win more coins if roll is one
+
+        }
+        else if (roll == 2){//win stuff if roll is 2
+
+        }
+        else{//you lose
+        
+        }
     }
   return 0;
 }
