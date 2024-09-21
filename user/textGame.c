06dadc4ef33a0c3c3bfa56858786ec3a127ed6d4
CB4747 <123422381+CB4747@users.noreply.github.com>
2024-09-20 17:47:01 -0700

    add values from chart

--- user/textGame.c
+++ user/textGame.c
@@ -6,10 +6,11 @@
 
 int LCG(int prev){// function that generates a psuedorandom number, using the previous value in the sequence, which starts at a seed
     prev = prev % m;
-    long a = 0;
-    long c = 0;
-    long m = 0;
-  return (a*prev+c)%m;
+    //https://www.ams.org/journals/mcom/1999-68-225/S0025-5718-99-00996-5/S0025-5718-99-00996-5.pdf has table of values
+    long a = 530877178;
+    long c = 0;//c = 0, since it is what the chart has
+    long m = 536870909;// 2^29 -3
+    return (a*prev+c)%m;
 }
 int main() {
 
