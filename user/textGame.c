8f7728bb49987acff46c469d2bc5ddee2f413285
CB4747 <123422381+CB4747@users.noreply.github.com>
2024-09-20 18:41:26 -0700

    fix using money instead of coins

--- user/textGame.c
+++ user/textGame.c
@@ -56,7 +56,7 @@ int main() {// a reimplementation of a simple casino game I wrote in python a wh
             printf('you have %d things', stuff);
         }
         else{//you lose
-            money=money-atoi(betInput);// lose your bet
+            coins=coins-atoi(betInput);// lose your bet
             printf("you lose");
             
         }
